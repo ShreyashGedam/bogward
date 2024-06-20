b0f5f7a2185d2dd63f20bc1efaff6355eb80549f
shreyash-attryb <shreyash@attryb.com>
2024-06-20 21:04:51 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { connect } = require("./db");
 const userRouter = require("./controllers/user.controller");
+var cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors()); 
 app.use(express.json());
 
 app.use("/", userRouter);
