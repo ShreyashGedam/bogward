db637257f5610460d3529c0d8964ec4e44b21f46
shreyash-attryb <shreyash@attryb.com>
2024-06-20 20:55:35 +0530

    changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,10 +22,42 @@ userRouter.post("/signup", async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      status: true,
+      message: "Signed up successfully",
     });
   } catch (error) {
     console.log(error);
-    return res.status(400).json(error);
+    return res.status(400).json({
+      status: false,
+      message: "Signup Failed",
+    });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, mobile } = req.body;
+
+  try {
+    const user = await UserModal.findOne({ email, mobile });
+
+    if (!user) {
+      return res.status(401).send({ message: "Invalid email or mobile" });
+    }
+
+    res.status(200).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      mobile: user.mobile,
+      status: true,
+      message: "Logged in successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: false,
+      message: "Login Failed",
+    });
   }
 });
 
